8bb3451c8defeba793be01109f9cfb0c0e148524
Terry Allitt <tallitt88@gmail.com>
2018-04-22 08:43:15 +0100

    :construction: Add event handlers for clicking add button or pressing enter key

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
+  // Some code
 
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
   // Some code
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
+  var cntrlAddItem = function() {
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
+    // 1. get input data
+    // 2. Add the item to the budget controller
+    // 3. Add the new item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget
+    console.log('it works');
+
   }
+
+  document.querySelector('.add__btn').addEventListener('click', cntrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+
+    if (event.code === "Enter") {
+      cntrlAddItem();
+    }
+
+  });
 
 })(budgetController, UIController);
