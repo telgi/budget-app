8844af833f476981a177908ff7f30092025a00b7
Terry Allitt <tallitt88@gmail.com>
2018-04-22 19:31:17 +0100

    :bug: Data only stored if there is string input for description and positive integer input for value

--- app.js
+++ app.js
@@ -44,10 +44,6 @@ var budgetController = (function() {
       // return the new element
       return newItem;
     },
-
-    testing: function() {
-      console.log(data);
-    }
   };
 
 })();
@@ -69,7 +65,7 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       }
     },
 
@@ -131,23 +127,27 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
   };
 
+  var updateBudget = function() {
+    // 1. Calculate the budget
+    // 2. Return the budget
+    // 3. Display the budget
+  };
+
   var cntrlAddItem = function() {
     var input, newItem;
     // 1. get input data
     input = UICtrl.getInput();
 
-    // 2. Add the item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-
-    // 3. Add the new item to the UI
-    UICtrl.addListItem(newItem, input.type);
-
-    // 4. Clear the fields
-    UICtrl.clearFields();
-
-    // 5. Calculate the budget
-    // 6. Display the budget
-
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      // 3. Add the new item to the UI
+      UICtrl.addListItem(newItem, input.type);
+      // 4. Clear the fields
+      UICtrl.clearFields();
+      // 5. Calculate and update budget
+      updateBudget();
+    }
   };
 
   return {
