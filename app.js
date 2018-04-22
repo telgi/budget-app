45f2dc4b866384e2a17b4fcb6399d52f7de154c6
Terry Allitt <tallitt88@gmail.com>
2018-04-22 18:46:08 +0100

    :lipstick: Clear the input fields after input has been entered

--- app.js
+++ app.js
@@ -93,6 +93,20 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArray;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+      fieldsArray = Array.prototype.slice.call(fields);
+
+      fieldsArray.forEach(function(current, index, array) {
+        current.value = '';
+      });
+
+      fieldsArray[0].focus();
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -128,8 +142,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 3. Add the new item to the UI
     UICtrl.addListItem(newItem, input.type);
 
-    // 4. Calculate the budget
-    // 5. Display the budget
+    // 4. Clear the fields
+    UICtrl.clearFields();
+
+    // 5. Calculate the budget
+    // 6. Display the budget
 
   };
 
