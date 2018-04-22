43fa8e1e04316360ae0b2135c208299b56c44222
Terry Allitt <tallitt88@gmail.com>
2018-04-22 10:54:12 +0100

    :construction: UIController grabs input from user and passes to Global Controller for event handling

--- app.js
+++ app.js
@@ -8,25 +8,48 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-  // Some code
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn'
+  }
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMstrings();
+
   var cntrlAddItem = function() {
 
     // 1. get input data
+    var input = UICtrl.getInput();
+    console.log(input);
+
     // 2. Add the item to the budget controller
     // 3. Add the new item to the UI
     // 4. Calculate the budget
     // 5. Display the budget
-    console.log('it works');
 
   }
 
-  document.querySelector('.add__btn').addEventListener('click', cntrlAddItem);
+  document.querySelector(DOM.inputButton).addEventListener('click', cntrlAddItem);
 
   document.addEventListener('keypress', function(event) {
 
